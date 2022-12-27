237c1cb39b9c64bb1b83c479a07696b8c001bf09
JosiMacGyver <joseana.stos.soares@gmail.com>
2022-12-27 11:59:37 -0300

    refatorando useReducer

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,15 +1,70 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+import { initialState, reducer } from '../../store'
+import { login, numberAdd2 } from '../../store/actions'
+
+//initial state e function passada para store/index.js 
+
 const UseReducer = (props) => {
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
     return (
         <div className="UseReducer">
             <PageTitle
                 title="Hook UseReducer"
                 subtitle="Uma outra forma de ter estado em componentes funcionais!"
             />
+            <div className="center">
+
+                {state.user ?
+                    <span className="text">
+                        {state.user.name}
+                    </span>
+                    :
+                    <span className="text">
+                        Sem Usuário
+                    </span>
+                }
+
+                <span className="text">
+                    {state.number}
+                </span>
+                <div>
+                    <button
+                        className="btn"
+                        onClick={() => login(dispatch, 'Josi')}>
+                        Login
+                    </button>
+                    <button
+                        className="btn"
+                        onClick={() => numberAdd2(dispatch)}>
+                        +2
+                    </button>
+                    <button
+                        className="btn"
+                        onClick={() => dispatch({ type: 'numberMulti7' })}>
+                        x7
+                    </button>
+                    <button
+                        className="btn"
+                        onClick={() => dispatch({ type: 'numberDiv25' })}>
+                        /25
+                    </button>
+                    <button
+                        className="btn"
+                        onClick={() => dispatch({ type: 'numberInt' })}>
+                        Int
+                    </button>
+                    <button
+                        className="btn"
+                        onClick={() => dispatch({ type: 'numberAddN', payload: +10 })}>
+                        +10
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
-
 export default UseReducer
